94f9f67f477a435c93f36ecf1738fc3eef8d29f9
LewisCM14 <Lewiscmartin@live.co.uk>
2022-11-30 23:37:33 +0000

    begin Todo form & list components

---
+++ src/components/TodoForm.vue
@@ -0,0 +1,12 @@
+<template>
+    <h1></h1>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+@Options({})
+export default class TodoForm extends Vue{
+    
+}
+</script>
