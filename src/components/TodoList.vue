ed6517c23c833ec54052655bfcd9f74077c75177
LewisCM14 <Lewiscmartin@live.co.uk>
2022-12-01 23:30:07 +0000

    begin rendering items in the TodoList

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,25 +1,44 @@
 <template>
-    <h1>Todo List</h1>
-    <todo-form @send-message="createTodo"></todo-form>
-</template> 
+    <el-row>
+        <el-col :span="12" :offset="7" style="width: 100%">
+            <h1>Todo List</h1>
+            <todo-form @send-message="createTodo"></todo-form>
+            <el-table :data="todos">
+                <el-table-column prop="title" table="Title" width="350"/>
+                <el-table-column prop="completed" label="Completed" width="200"/>
+            </el-table>
+        </el-col>
+    </el-row>
+</template>
 
 <script lang="ts">
-import { ElMessage } from 'element-plus';
-import { Options, Vue } from 'vue-class-component';
-import TodoForm from './TodoForm.vue';
+import { ElMessage } from "element-plus";
+import { Options, Vue } from "vue-class-component";
+import TodoForm from "./TodoForm.vue";
 
 @Options({
     components: {
         TodoForm,
-    }
+    },
 })
-export default class TodoList extends Vue{
-    async createTodo(todo: any){
-        console.log("Todo", todo)
+export default class TodoList extends Vue {
+    todos = [];
+
+    async mounted() {
+        await this.loadTodos();
+    }
+
+    async loadTodos() {
+        const response = await this.axios.get(`http://localhost:8000/todos`);
+        this.todos = response.data;
+    }
+
+    async createTodo(todo: any) {
+        console.log("Todo", todo);
         ElMessage({
             message: "Todo Created",
             type: "success",
-        })
+        });
     }
 }
 </script>
