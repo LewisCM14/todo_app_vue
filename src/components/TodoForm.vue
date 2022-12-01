c4ab2979fba1f63c5ff9e700f35ae2e13357a71e
LewisCM14 <Lewiscmartin@live.co.uk>
2022-12-01 22:21:44 +0000

    create TodoForm template

--- src/components/TodoForm.vue
+++ src/components/TodoForm.vue
@@ -1,5 +1,12 @@
 <template>
-    <h1></h1>
+    <el-form @submit.prevent :inline="true" :model="formInput" class="todo-form-input">
+        <el-form-item label="Title" prop="title">
+            <el-input v-model="formInput.title" placeholder="Enter Todo"></el-input>
+        </el-form-item>
+        <el-form-item>
+            <el-button type="primary" @click="onSubmit()">Add</el-button>
+        </el-form-item>
+    </el-form>
 </template>
 
 <script lang="ts">
@@ -7,6 +14,10 @@ import { Options, Vue } from 'vue-class-component';
 
 @Options({})
 export default class TodoForm extends Vue{
-    
+    formInput = { title: "", completed: false }
+
+    onSubmit(){
+        console.log("Todo", this.formInput)
+    }
 }
 </script>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,11 +1,17 @@
 <template>
     <h1>Todo List</h1>
+    <todo-form></todo-form>
 </template> 
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
+import TodoForm from './TodoForm.vue';
 
-@Options({})
+@Options({
+    components: {
+        TodoForm,
+    }
+})
 export default class TodoList extends Vue{
     
 }
